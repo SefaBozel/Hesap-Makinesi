45d16a6cc95acd0032e98f47867ab9aaf02d8e61
SefaBozel <sefabozel57@gmail.com>
2024-06-05 21:39:15 +0300

    Add files via upload

---
+++ Hesap Makinesi/hesap-makinesi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arac
+{
+    public partial class hesap_makinesi : Form
+    {
+        public hesap_makinesi()
+        {
+            InitializeComponent();
+        }
+
+        private void hesap_makinesi_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("+");
+            comboBox1.Items.Add("-");
+            comboBox1.Items.Add("*");
+            comboBox1.Items.Add("/");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double hesapla;
+            int sayi1, sayi2;
+            string islem;
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+            islem = comboBox1.Text;
+            switch (islem)
+            {
+                case "+": hesapla = sayi1 + sayi2; label4.Text = hesapla.ToString(); break;
+                case "-": hesapla = sayi1 - sayi2; label4.Text = hesapla.ToString(); break;
+                case "*": hesapla = sayi1 * sayi2; label4.Text = hesapla.ToString(); break;
+                case "/": hesapla = sayi1 / sayi2; label4.Text = hesapla.ToString(); break;
+            }
+        }
+    }
+}
